d233dee6e8a35b781e4f58457a958b212fcd4733
Mike543 <p890098@gmail.com>
2022-01-25 13:48:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
+    let result = "";
+    let n = 0;
+    for (let i = 0; i < (expr.length / 10); i++, n += 10) {
+      let char = "";
+      let code = expr.substr(n, 10);
+      if (code[0] == "*") {
+        result += " ";
+        continue;
+      }
+      let j = 0;
+      while (code[j] == "0") j++;
+      j++;
+      for (j; j < 10; j += 2) {
+        if (code[j] == 0) {
+          char += ".";
+        } else {
+          char += "-";
+        }
+      }
+      result += MORSE_TABLE[char];
+    }
+    return result;
+  }
 module.exports = {
     decode
 }
